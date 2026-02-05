6c415e0737ccd9d208c3482be76beb43c4a6c04f
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2026-02-05 11:13:41 +0100

    snack-1

---
+++ main.js
@@ -0,0 +1,22 @@
+/*🏆 Snack 1
+Crea una funzione che somma due numeri.
+Crea una funzione dichiarativa chiamata somma che accetta due numeri e restituisce la loro somma.
+Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile
+Quindi, riscrivi la funzione somma con la sintassi delle arrow functions.*/
+
+/*
+function somma(a, b) {
+    console.log(a + b)
+}
+somma(3, 4)
+*/
+/*
+const somma = function (a, b) {
+    console.log(a + b)
+}
+
+somma(4, 7)
+*/
+
+const somma = (a, b) => { console.log(a + b) }
+somma(5, 5)
