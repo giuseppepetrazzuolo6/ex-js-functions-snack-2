f93f4be20977ea4b087914212bc17420a982406f
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2026-02-05 12:23:28 +0100

    snack-5

--- main.js
+++ main.js
@@ -61,3 +61,22 @@ function creaTimer(tempo) {
 }
 const timer = creaTimer(3000)
 timer()
+
+/*🏆 Snack 5
+Crea una funzione stampaOgniSecondo con setInterval.
+Definisci una funzione che accetta un messaggio e lo stampa ogni secondo.
+Nota: Questa funzione creerà un loop infinito. 
+Interrompilo manualmente o usa clearInterval() in un altro script.*/
+
+function stampaOgniSecondo(messaggio) {
+    return setInterval(() => {
+        console.log(messaggio)
+    }, 1000)
+}
+
+const intervallo = stampaOgniSecondo('Ciao!')
+
+setTimeout(() => {
+    clearInterval(intervallo)
+    console.log('Stampa conclusa.')
+}, 5000);
