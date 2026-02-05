f9ead97ee7c8007778b5e4c08e7f7f80646384bb
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2026-02-05 13:31:56 +0100

    snack-7

--- main.js
+++ main.js
@@ -102,3 +102,20 @@ const interval = tempo()
 setTimeout(() => {
     clearInterval(interval)
 }, 11000)
+
+/*🏆 Snack 7
+Crea una funzione che ferma un timer dopo un certo tempo
+Scrivi una funzione eseguiEferma che accetta un messaggio, 
+un tempo di avvio e un tempo di stop. Il messaggio deve essere stampato a intervalli regolari, 
+ma si deve fermare dopo il tempo di stop.*/
+
+function eseguiEferma(messaggio, avvio, stop) {
+    const id = setInterval(() => {
+        console.log(messaggio)
+    }, avvio)
+    setTimeout(() => {
+        clearInterval(id)
+    }, stop)
+}
+
+eseguiEferma('Sono Giuseppe', 1000, 10000)
